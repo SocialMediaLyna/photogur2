bbaebac2073ad2ca1bf9c5d16265b77b5fd24041
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-28 11:26:12 -0500

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,46 +3,46 @@ class PicturesController < ApplicationController
   def index
     @picture = Picture.all
   end
-
-  def show
-    @pictures = Picture.find(params[:id])
-    @comments = @picture.comments
-  end
-
-  def new
-    @picture = Picture.new
-  end
-
-  @picture = Picture.find(params[:id])
-  end
-
-  def update
-    @picture = Picture.find(params[:id])
-
-    if @picture.update_attributes(picture_params)
-      redirect_to "/pictures/#{@picture.id}"
-    else
-      render :edit
-  end
-end
-
-  def create
-    @picture = Picture.new(picture_params)
-    if @picture.save
-      redirect_to pictures_url
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def picture_params
-    params.require(:picture).permit(:artist, :title, :url)
-  end
-
-  def destroy
-    @picture = Picture.find(params[:id])
-    @picture.destroy
-      redirect_to pictures_url
+#
+#   def show
+#     @pictures = Picture.find(params[:id])
+#     @comments = @picture.comments
+#   end
+#
+#   def new
+#     @picture = Picture.new
+#   end
+#
+#   @picture = Picture.find(params[:id])
+#   end
+#
+#   def update
+#     @picture = Picture.find(params[:id])
+#
+#     if @picture.update_attributes(picture_params)
+#       redirect_to "/pictures/#{@picture.id}"
+#     else
+#       render :edit
+#   end
+# end
+#
+#   def create
+#     @picture = Picture.new(picture_params)
+#     if @picture.save
+#       redirect_to pictures_url
+#     else
+#       render :new
+#     end
+#   end
+#
+#   private
+#
+#   def picture_params
+#     params.require(:picture).permit(:artist, :title, :url)
+#   end
+#
+#   def destroy
+#     @picture = Picture.find(params[:id])
+#     @picture.destroy
+#       redirect_to pictures_url
 end
